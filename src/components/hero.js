10543fd47455cb9f751a88c4b5c5fbd2b86fa614
StefanL <78504230+stefanlinde95@users.noreply.github.com>
2022-01-19 22:54:15 +0200

    Add files via upload

---
+++ src/components/hero.js
@@ -0,0 +1,22 @@
+import * as React from "react"
+
+const Hero = () => (
+  <section id="hero">
+    <div className="container-fluid">
+      <div className="row">
+        <div id="rennid" className="col-6">
+          <a href="#" className="hero-text">
+            <h2>Rennide puhastus</h2>
+          </a>
+        </div>
+        <div id="fassaadipesu" className="col-6">
+          <a href="#" className="hero-text">
+            <h2>Fassaadipesu</h2>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+)
+
+export default Hero
