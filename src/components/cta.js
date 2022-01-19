57ad3e492f726e7697f894ed214ef0348bbdfd6a
stefanl <stefanlinde1995@gmail.com>
2022-01-19 22:57:00 +0200

    third

---
+++ src/components/cta.js
@@ -0,0 +1,23 @@
+import * as React from "react"
+
+const Cta = () => (
+  <section id="cta" className="mb-5">
+    <div className="container py-5">
+      <div className="row py-5">
+        <div className="col-7">
+          <a href="#" className="cta-text">
+            <h2>Soovid ka oma fassadi puhtaks saada?</h2>
+            <p>Siis küsi meie käest julgelt hinnapakkumist!</p>
+          </a>
+        </div>
+        <div className="col-5 d-flex cta-btn-wrapper">
+          <button className="btn btn-info">
+            <a href="#">Küsi hinnapakkumist</a>
+          </button>
+        </div>
+      </div>
+    </div>
+  </section>
+)
+
+export default Cta
